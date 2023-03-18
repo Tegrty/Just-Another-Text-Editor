7e00f16eb666a252ca622b4e5fb4f87d8ec02137
Mario Repas <mario.repas63@gmail.com>
2023-03-17 20:35:15 -0400

    debugged dist folder not being created.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,8 @@
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // added mini css extract plugin for css extraction
-const WorkboxPlugin = require('workbox-webpack-plugin'); // added workbox plugin for service worker and manifest file generation.
-
-
 
 module.exports = () => {
   return {
@@ -19,24 +16,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin({template: "./index.html", title: "JATE"}),
-      new WebpackPwaManifest({ fingerprints: false, name: 'Just Another Text Editor', short_name: 'JATE', description: 'Somewhere to type things', background_color: '#01579b', theme_color: '#ffffff', start_url: '/', icons: [{ src: path.resolve('src/images/icon.png'), sizes: [96, 128, 192, 256, 384, 512] }] }),
-      new InjectManifest({ swSrc: './src/sw.js', swDest: 'src-sw.js' }),
-      new MiniCssExtractPlugin()
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
-
     module: {
       rules: [
-        {test: /\.css$/i, use: ["style-loader", "css-loader"]},
-        {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource"},
-        {test: /\.m?js$/, exclude: /node_modules/, use: {
-            loader: "babel-loader",
-            options: {presets: ["@babel/preset-env"],plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-                "@babel/plugin-transform-runtime"
-              ]}
-          }
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
         },
       ],
     },
