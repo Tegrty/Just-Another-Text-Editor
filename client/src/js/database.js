04a1791edf30c5b9e9ff6871bd5dae2aed9ffeac
Mario Repas <mario.repas63@gmail.com>
2023-03-19 11:46:52 -0400

    added putDB and getDb functions to database to be able to write and retrieve data

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,38 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
+import { NoEmitOnErrorsPlugin } from "webpack";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const id = await store.add(content);
+  await tx.complete;
+  return id;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const allContent = await store.getAll();
+  await tx.complete;
+  return allContent;
 
-initdb();
+
+initdb(); 
+
+// The putDb function creates a connection to the "jate" database, starts a read-write transaction on the "jate" object store, adds the content to the object store using the add method, waits for the transaction to complete using await tx.complete, and returns the id of the newly added object.
+
+// The getDb function also creates a connection to the "jate" database, starts a read-only transaction on the "jate" object store, retrieves all the content using the getAll method, waits for the transaction to complete using await tx.complete, and returns all the content in the "jate" object store. 
